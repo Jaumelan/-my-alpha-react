48a6e45adc7f6cae8c0b5328507a8b0f970855bc
Jaime <jaume.lan@gmail.com>
2022-04-14 06:54:44 -0300

    Atividade React parte 03

---
+++ src/components/Screen.jsx
@@ -0,0 +1,12 @@
+//import { Textfit } from "react-textfit"
+import "./Screen.css";
+
+const Screen = ({ value }) => {
+    return (
+        <p className="screen" mode="single" max={70}>
+        {value}
+      </p>
+    );
+  };
+  
+  export default Screen;
